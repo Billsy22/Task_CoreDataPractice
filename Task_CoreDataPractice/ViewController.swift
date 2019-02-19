3e0a6bd8e6188e7affce8be9da54c2f5404482e2
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2019-02-19 13:02:26 -0700

    Initial Commit

---
+++ Task_CoreDataPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task_CoreDataPractice
+//
+//  Created by Taylor Bills on 2/19/19.
+//  Copyright © 2019 Taylor Bills. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
