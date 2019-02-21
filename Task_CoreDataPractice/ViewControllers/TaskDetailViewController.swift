3fea05efd1ab9f21e19706cc8b411cb689b00d2a
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2019-02-20 19:33:22 -0700

    Set up TaskDetailViewController.

--- Task_CoreDataPractice/ViewControllers/TaskDetailViewController.swift
+++ Task_CoreDataPractice/ViewControllers/TaskDetailViewController.swift
@@ -13,23 +13,33 @@ class TaskDetailViewController: UIViewController {
     // MARK: -  Properties
     
     var task : Task?
+    var dueDate: Date?
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var notesTextView: UITextView!
     
     // MARK: -  Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: -  Actions
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let nameText = nameTextField.text, let notesText = notesTextView.text else { return }
+        if task == nil {
+            TaskController.shared.addTask(withName: nameText, notes: notesText, due: Date())
+        } else {
+            TaskController.shared.update(task: self.task!, name: nameText, notes: notesText, due: Date())
+        }
+        navigationController?.popViewController(animated: true)
     }
-    */
-
+    
+    func updateViews() {
+        if task != nil {
+            nameTextField.text = task!.name
+            notesTextView.text = task!.notes
+        }
+    }
 }
